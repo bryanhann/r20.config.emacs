69004f193a0009964376653bd23d829003fba217
Bryan Hann <nobody@nowhere>
2020-04-22 11:33:48 +0800

    Initial commit

---
+++ init.el
@@ -0,0 +1,9 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+; M-x package-refresh-contents RET
+; M-x package-install RET magit RET
+;(load "~/.config/r20/emacs/hydra/lv.el")
+;(load "~/.config/r20/emacs/spacemacs/init.el")
+
