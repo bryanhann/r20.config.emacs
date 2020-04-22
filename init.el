4dfd6636bf635d900440dfb86d72792cf71555c8
Bryan Hann <nobody@nowhere>
2020-04-22 11:42:08 +0800

    Add shortcuts for magit

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
+(global-set-key (kbd "C-x g") 'magit-status)
 ; M-x package-refresh-contents RET
 ; M-x package-install RET magit RET
 ;(load "~/.config/r20/emacs/hydra/lv.el")
